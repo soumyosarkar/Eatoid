a68679e8bdc083dcda264cdab7c3ab298b538b67
Soumyo <soumyosarkar001@gmail.com>
2025-01-25 12:07:51 +0530

    showing orders in admin panel

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from "axios"
+
+const List = () => {
+    const [list, setList] = useState([])
+    const fetchlist = async ()=>{
+        const response = await axios
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default List
